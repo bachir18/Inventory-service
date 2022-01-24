7eee85ef595512ad22f534ddd9a83857f5a2f203
bachir18 <75697872+bachir18@users.noreply.github.com>
2022-01-24 22:55:42 +0100

    My first commit

---
+++ inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,29 @@
+package org.sid.inventoryservice;
+
+import org.sid.inventoryservice.entities.Product;
+import org.sid.inventoryservice.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+@Bean
+    CommandLineRunner start(ProductRepository productRepository, RepositoryRestConfiguration repositoryRestConfiguration){
+        repositoryRestConfiguration.exposeIdsFor(Product.class);
+        return args -> {
+            productRepository.save(new Product(null,"Article1",555,2));
+            productRepository.save(new Product(null,"Article2",5655,54));
+            productRepository.save(new Product(null,"Article3",5558,56));
+            productRepository.findAll().forEach(product -> {
+                System.out.println(product.getName());
+            });
+        };
+    }
+}
